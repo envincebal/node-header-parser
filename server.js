f9dfe94371b5c3e56637d5186d3e6ba2573032fc
envincebal <vincentyan1@gmail.com>
2018-07-12 21:03:37 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+var getClientAddress = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
+console.log(__dirname + "/node_modules");
+  res.send({
+    ipaddress: getClientAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
